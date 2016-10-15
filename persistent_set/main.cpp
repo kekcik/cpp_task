79d08eff07efe84af6550cee2fd54400a6100d9c
Ivan Trofimov <richard1997dog@gmail.com>
2016-10-15 08:58:44 +0300

    Initial Commit

---
+++ persistent_set/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  persistent_set
+//
+//  Created by Иван Трофимов on 15.10.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
